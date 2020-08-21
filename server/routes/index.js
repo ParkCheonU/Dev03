0b6d7773ebc3209ec8f659280999b61124e5f78f
pangcurry <dupang24@dsm.hs.kr>
2020-08-21 14:53:21 +0900

    edit/신두평/서버index.js 수정

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ const upload = require("../middleware/multer").upload;
 const router = express.Router();
 
 router.post("/writing", upload.single("img"), postController.writing);
-router.get("/upload", postController.upload);
-router.post("/like", postController.like); 
+router.post("/upload", postController.upload);
+router.post("/like", postController.like);
 
 module.exports = router;
